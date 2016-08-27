44da9f2115ca1af57ae95610630d74373e93fc1f
kagasu <admin@kagasu.net>
2016-08-27 20:11:00 +0900

    Initial commit

---
+++ UnityWebPlayerFullScreen/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnityWebPlayerFullScreen
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var screens = Screen.AllScreens;
+            listViewDisplays.DataContext = screens;
+            
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    listViewUnityWebPlayerWindows.Dispatcher.Invoke(() =>
+                    {
+                        var selectedIndex = listViewUnityWebPlayerWindows.SelectedIndex;
+                        listViewUnityWebPlayerWindows.DataContext = Process.GetProcesses().Where(x => x.MainWindowTitle.Contains("Unity Web Player")).ToArray();
+                        listViewUnityWebPlayerWindows.SelectedIndex = selectedIndex;
+                    });
+                    
+                    Thread.Sleep(1000);
+                }
+            });
+
+            button.Click += delegate(object sender, RoutedEventArgs e)
+            {
+                var display = (Screen)listViewDisplays.SelectedItem;
+                var process = (Process)listViewUnityWebPlayerWindows.SelectedItem;
+
+                if (display != null && process != null)
+                {
+                    WindowManager.ChangeBorderless(process.MainWindowHandle);
+                    WindowManager.ChangeFullScreen(process.MainWindowHandle, display.DeviceName);
+                }
+            };
+        }
+    }
+}
---
+++ UnityWebPlayerFullScreen/WindowManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnityWebPlayerFullScreen
+{
+    class WindowManager
+    {
+        const int WS_BORDER = 8388608;
+        const int WS_DLGFRAME = 4194304;
+        const int WS_CAPTION = WS_BORDER | WS_DLGFRAME;
+        const int WS_SYSMENU = 524288;
+        const int WS_THICKFRAME = 262144;
+        const int WS_MINIMIZE = 536870912;
+        const int WS_MAXIMIZEBOX = 65536;
+        const int GWL_STYLE = -16;
+        const int GWL_EXSTYLE = -20;
+        const int SWP_FRAMECHANGED = 0x20;
+
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+
+        public WindowManager()
+        {
+
+        }
+
+        public static void ChangeBorderless(IntPtr hWnd)
+        {
+            var style = GetWindowLong(hWnd, GWL_STYLE);
+            style &= ~WS_CAPTION & ~WS_SYSMENU & ~WS_THICKFRAME & ~WS_MINIMIZE & ~WS_MAXIMIZEBOX;
+
+            SetWindowLong(hWnd, GWL_STYLE, style);
+            style = GetWindowLong(hWnd, GWL_EXSTYLE);
+            SetWindowLong(hWnd, GWL_EXSTYLE, style);
+        }
+
+        public static void ChangeFullScreen(IntPtr hWnd, string deviceName)
+        {
+            foreach (var screen in Screen.AllScreens)
+            {
+                if (screen.DeviceName == deviceName)
+                {
+                    SetWindowPos(hWnd, IntPtr.Zero, screen.Bounds.X, screen.Bounds.Y, screen.Bounds.Width, screen.Bounds.Height, SWP_FRAMECHANGED);
+                    break;
+                }
+            }
+        }
+    }
+}
